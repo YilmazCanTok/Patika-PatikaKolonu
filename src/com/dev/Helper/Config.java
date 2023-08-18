e3b37b1658259fad57468469ace4073d649873c2
YılmazCanTOK <yilmazcantok89@gmail.com>
2023-08-18 23:25:10 +0300

    first commit

---
+++ src/com/dev/Helper/Config.java
@@ -0,0 +1,9 @@
+package com.dev.Helper;
+
+public class Config {
+    public static final String PROJECT_TITLE = "Patika.Dev";
+    public static final String DB_URL = "jdbc:postgresql://localhost:5432/Patika";
+    public static final String DB_USERNAME = "postgres";
+    public static final String DB_PASSWORD = "12345";
+
+}
